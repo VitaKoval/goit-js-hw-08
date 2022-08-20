c7d6fe26a2d0a4c2853938ed40bbc6f63c86e3a1
Vita Koval <mayevit@hotmail.com>
2022-08-20 17:36:40 +0300

    HW-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" 
+        href="${original}">
+        <img 
+        class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
