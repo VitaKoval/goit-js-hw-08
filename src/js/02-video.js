8c7283bc0d5bc968ef7471e8c3038f5d07992167
Vita Koval <mayevit@hotmail.com>
2022-08-22 16:47:33 +0300

    end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+populatePlayer();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+  const timePlay = evt.seconds;
+  console.log(timePlay);
+  localStorage.setItem('videoplayer-current-time', timePlay);
+}
+
+function populatePlayer() {
+  const saveTime = localStorage.getItem('videoplayer-current-time');
+  if (saveTime) {
+    player.setCurrentTime(saveTime);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('[type="email"]');
+const messageInput = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onDateInput, 500));
+
+const dataInput = {};
+
+populateData();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(dataInput);
+  // console.log({email: emailInput.value, message: messageInput.value})
+  // console.log(evt.currentTarget.elements);
+
+  evt.currentTarget.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onDateInput(evt) {
+  dataInput[evt.target.name] = evt.target.value;
+  // console.log(dataInput);
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataInput));
+}
+
+function populateData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  console.log(savedData);
+
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+
+    emailInput.value = parsedData.email;
+    messageInput.value = parsedData.message;
+
+    dataInput.email = parsedData.email;
+    dataInput.message = parsedData.message;
+    // console.log(dataInput)
+  }
+}
